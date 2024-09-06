2dde29c96ff4b705796dd5ed5179c2c587eb049a
Lubasi Milupi <lubasimilupi@gmail.com>
2024-09-06 15:41:14 +0200

    Updated all queries needed for the database schema

--- queries.sql
+++ queries.sql
@@ -3,27 +3,14 @@ CREATE TABLE books(
     title VARCHAR(100) NOT NULL,
     rating VARCHAR(20),
     details TEXT,
-    date_read TEXT
+    date_read TEXT,
+    image_url TEXT
 )
 
 CREATE TABLE notes(
     isbn VARCHAR(100) REFERENCES books(isbn) UNIQUE,
     report TEXT
 )
-
-ALTER TABLE books
-    ADD image_url TEXT
-
-UPDATE books
-SET image_url = 'https://covers.openlibrary.org/b/ISBN/9780008375010-L.jpg'
-WHERE title='Who They Was'
-
-SELECT * FROM books
-
-INSERT INTO books (isbn,title,rating,details,date_read,image_url)
-VALUES ($1,$2,$3,$4,$5,$6)
-
-
 
 
 --Sort by date (Most recent) --
@@ -43,6 +30,6 @@ ORDER BY title ASC;
 SELECT * FROM books
 ORDER BY title DESC;
 
--- UPDATE Query --
+
 
 
